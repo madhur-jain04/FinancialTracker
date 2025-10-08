171107d0b16ee4b6d1fd2fa7cc7d3d583ee21b5d
madhur-jain04 <jainmadhur0405@gmail.com>
2025-10-08 07:33:05 +0530

    Update Transaction.js

--- backend/models/Transaction.js
+++ backend/models/Transaction.js
@@ -1,32 +1,3 @@
-// // backend/models/Transaction.js
-// import mongoose from 'mongoose';
-
-// const transactionSchema = new mongoose.Schema({
-//     // Core Fields
-//     type: { type: String, enum: ['Income', 'Expense'], required: true },
-//     amount: { type: Number, required: true, min: 0.01 },
-//     date: { type: Date, default: Date.now },
-//     description: { type: String, trim: true },
-    
-//     // Categorization
-//     category: { 
-//         type: String, 
-//         required: true, 
-//         enum: [
-//             'Salary', 'Investment', 'Other Income',
-//             'Groceries', 'Rent', 'Utilities', 'Transportation', 'Dining Out', 'Entertainment', 'Other Expense'
-//         ]
-//     },
-    
-//     // Source tracking
-//     source: { type: String, enum: ['Manual', 'Receipt Image'], default: 'Manual' },
-    
-// }, { timestamps: true });
-
-// const Transaction = mongoose.model('Transaction', transactionSchema);
-
-// export default Transaction;
-
 import mongoose from "mongoose";
 
 const transactionSchema = new mongoose.Schema({
